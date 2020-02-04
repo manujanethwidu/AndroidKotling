a63301aea478355afee8c9f443f3ec3d580631bf
EDC <manujanethwidu@gmail.com>
2020-02-04 13:20:52 +0530

    Section 2 Vedio3

--- app/src/main/java/com/example/kotlingtutorial1/MainActivity.kt
+++ app/src/main/java/com/example/kotlingtutorial1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlingtutorial1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         btnShowToast.setOnClickListener{
             Log.i("Main Activity","Button was clicked")
             Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgtoNextActivity.setOnClickListener{
+
+            val message:String = etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
